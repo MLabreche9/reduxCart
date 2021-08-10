9b78b2f9130eb4bc3644caacf69005eaa0a39c37
Meghan La Breche <Megavictoria@gmail.com>
2021-08-09 20:33:03 -0700

    fetch

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import store from '.';
 
 const cartSlice = createSlice({
 	name: 'cart',
@@ -7,6 +8,10 @@ const cartSlice = createSlice({
 		totalQuantity: 0
 	},
 	reducers: {
+		replaceCart(state, action) {
+			state.totalQuantity = action.payload.totalQuantity;
+			store.items = action.payload.items;
+		},
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find((item) => item.id === newItem.id);
